782f758d89430aa1a5a6b6dcf9eed3998b329acd
Alex <seredin-alex@mail.ru>
2022-10-18 20:29:23 +0300

    Задача 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,42 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-        System .out.println("Урок 10. Массивы часть 2. Задача 1");
+        System.out.println("Урок 10. Массивы часть 2. Задача 1");
         int sum = 0;
-        int [] arr = generateRandomArray();
+        int[] arr = generateRandomArray();
         for (int value : arr) {
             sum += value;
         }
-        System.out.println("Сумма трат за месяц составила "+ sum + " рублей");
-    }
-    int[] arr = generateRandomArray();
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
+        System.out.println();
+        System.out.println("Урок 10. Массивы часть 2. Задача 2");
+        arr = generateRandomArray();
+        int min = arr[0];
+        int max = arr[0];
+        for (int element : arr) {
+            if (element < min) {
+                min = element;
+            } else if (element > max) {
+                max = element;
+            }
         }
-        return arr;
+        System.out.println("Минимальная трата за день " + min);
+        System.out.println("Максимальная трата за день " + max);
+        System.out.println();
+        System.out.println("Урок 10. Массивы часть 2. Задача 3");
+        arr = generateRandomArray();
     }
+
+        int[] arr = generateRandomArray();
+
+        public static int[] generateRandomArray () {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
+
 }
