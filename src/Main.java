3906ec77d61e360c11b44cc6b8d5c0ceb6e8b780
Alex <seredin-alex@mail.ru>
2022-10-20 20:29:28 +0300

    Исправил расположение импорта

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println();
@@ -45,7 +46,7 @@ public class Main {
         int[] arr = generateRandomArray();
 
         public static int[] generateRandomArray () {
-            java.util.Random random = new java.util.Random();
+            Random random = new Random();
             int[] arr = new int[30];
             for (int i = 0; i < arr.length; i++) {
                 arr[i] = random.nextInt(100_000) + 100_000;
